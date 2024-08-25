b46d5c8dcd00bed50d1112e1546a4ab04b36fef0
Harshit <harshityadav.0025@gmail.com>
2024-08-25 13:08:39 +0530

    ...

---
+++ test/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [data, setData] = useState("");
+  const [response, setResponse] = useState({});
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:5000/bfhl", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: data,
+      });
+      const xyz = await res.json();
+      console.log(xyz);
+
+      setResponse(data);
+    } catch (err) {
+      setError("Invalid JSON or Network Error");
+    }
+  };
+
+  return (
+    <div>
+        <h1></h1>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          placeholder="Enter JSON data here"
+          rows="5"
+          cols="50"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Filtered Response</p>
+      {error && <p>{error}</p>}
+      {response && <p>{JSON.stringify(response)}</p>}
+    </div>
+  );
+}
